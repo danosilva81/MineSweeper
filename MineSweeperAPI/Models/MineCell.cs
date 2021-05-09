d983179962154cba8896b6d7ccd22aadb31e9d8c
Daniel Silva <djimenez@qubeyond.com>
2021-05-09 12:18:07 -0300

    adding id generator to cells

--- MineSweeperAPI/Models/MineCell.cs
+++ MineSweeperAPI/Models/MineCell.cs
@@ -1,11 +1,12 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
 
 namespace MineSweeperAPI.Models
 {
     public class MineCell
     {
-        [BsonId]
+        [BsonId(IdGenerator = typeof(ObjectIdGenerator))]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
 
